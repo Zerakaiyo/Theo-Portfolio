f85779f4e4ad0b7e377737db30e0e514fdad21d5
ForsakendMJr <64882709+ForsakendMJr@users.noreply.github.com>
2026-06-11 07:50:17 +0100

    Flush

--- MediaCard.js
+++
@@ -1,9 +0,0 @@
-import ProjectCard from './ProjectCard';
-
-export default function ProjectGrid({ projects, tall = false, driveUrl }) {
-  return (
-    <div className={`mt-10 grid gap-5 ${tall ? 'sm:grid-cols-2 lg:grid-cols-4' : 'md:grid-cols-2 xl:grid-cols-3'}`}>
-      {projects.map((project) => <ProjectCard key={project.title} project={project} tall={tall} driveUrl={driveUrl} />)}
-    </div>
-  );
-}
--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-# Theo Portfolio
-
-Next.js portfolio for Theo Majer with embedded Google Drive previews.
-
-Edit portfolio content in `data/projects.js`.
